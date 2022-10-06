ab7543c5c6a0f7e04a0b33b92999c03afcf5cd69
uku <uku3lig@gmail.com>
2022-10-06 18:19:21 +0200

    change python script to download fonts too

--- download_grapic.py
+++ download_grapic.py
@@ -1,17 +1,19 @@
 import os
 import urllib.request
 
-BASE_URL = "https://raw.githubusercontent.com/ucacaxm/grapic/master/src"
-FILES = ["Grapic.cpp", "Grapic.h"]
+BASE_URL = "https://raw.githubusercontent.com/ucacaxm/grapic/master"
+FILES = ["src/Grapic.cpp", "src/Grapic.h", 'data/ttf/Roboto-Regular.ttf']
 
-os.makedirs("src", exist_ok=True)
 for fname in FILES:
     print(f"Downloading '{fname}'...")
-    url = "/".join([BASE_URL, fname])
 
+    os.makedirs("/".join(fname.split("/")[:-1]), exist_ok=True)
+
+    url = "/".join([BASE_URL, fname])
     res = urllib.request.urlopen(url)
+
     if (res.status == 200):
-        open(f'src/{fname}', "wb").write(res.read())
+        open(fname, "wb").write(res.read())
     else:
         print(f"There was an error downloading {fname} (error {res.status})")
 
