dcb39d0851218d1073ed89038f790e98eb7cca58
uku <uku3lig@gmail.com>
2022-10-12 22:02:41 +0200

    make setup.sh 'bash -c'-able

--- setup.sh
+++ setup.sh
@@ -2,17 +2,22 @@
 
 if [[ -z $1 ]];
 then 
-    echo "You need to pass a directory name."
-    exit 1
+    echo "No directory name given, using '.'"
+    GRAPIC_DIRNAME="."
+else
+    GRAPIC_DIRNAME=$1
 fi
 
-git clone --depth 1 https://github.com/uku3lig/grapic-standalone "$1"
-cd "$1" || exit
+git clone --depth 1 https://github.com/uku3lig/grapic-standalone "$GRAPIC_DIRNAME"
+cd "$GRAPIC_DIRNAME" || exit
 
 bash download_grapic.sh
 rm -rf .git .gitignore LICENSE README.md setup.sh download_grapic.sh
 
-mv main.cpp "$1.cpp"
-sed -i -e "s/main/$1/g" Makefile
+if [[ -n $1 ]];
+then
+    mv main.cpp "$1.cpp"
+    sed -i -e "s/main/$1/g" Makefile
+fi
 
-echo "Project setup in directory '$1'"
+echo "Project setup in directory '$GRAPIC_DIRNAME'"
